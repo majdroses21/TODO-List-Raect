95c6b12d6109a26ee336faf7be6889d2b96f6b22
Majd Roses <majdrose@gmail.com>
2026-01-05 18:29:14 +0300

    The theme was moved to a separate file to clean the code and make it professional according to international standards.

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,9 @@
 import "./App.css";
 import ToDoList from "./components/ToDoList";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Checkbox from "@mui/material/Checkbox";
-//Colors need to use in my App
-import { purple, blueGrey } from "@mui/material/colors";
-//theeme one
-const IndigoBlueTheme = createTheme({
-  palette: {
-    primary: {
-      light: "#757ce8",
-      main: "#3f50b5",
-      dark: "#002884",
-      contrastText: "#fff",
-    },
-    secondary: {
-      light: "#ff7961",
-      main: "#f44336",
-      dark: "#ba000d",
-      contrastText: "#000",
-    },
-  },
-});
-//another theme
-const purpleTheme = createTheme({
-  palette: {
-    primary: {
-      main: purple[500],
-      light: purple[100],
-      dark: purple[700],
-      contrastText: "#ffffff",
-    },
-    secondary: {
-      main: blueGrey[500],
-      light: blueGrey[100],
-      dark: blueGrey[700],
-      contrastText: "#ffffff",
-    },
-  },
-});
+import { ThemeProvider } from "@mui/material/styles";
+import { IndigoBlueTheme,purpleTheme } from "./theme/theme";
+
 
 function App() {
   return (
---
+++ src/theme/theme.js
@@ -0,0 +1,38 @@
+import { createTheme } from "@mui/material/styles";
+import { purple, blueGrey } from "@mui/material/colors";
+
+// Indigo Blue Theme
+export const IndigoBlueTheme = createTheme({
+  palette: {
+    primary: {
+      light: "#757ce8",
+      main: "#3f50b5",
+      dark: "#002884",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#f44336",
+      dark: "#ba000d",
+      contrastText: "#000",
+    },
+  },
+});
+
+// purple Theme
+export const purpleTheme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+      light: purple[100],
+      dark: purple[700],
+      contrastText: "#ffffff",
+    },
+    secondary: {
+      main: blueGrey[500],
+      light: blueGrey[100],
+      dark: blueGrey[700],
+      contrastText: "#ffffff",
+    },
+  },
+});
