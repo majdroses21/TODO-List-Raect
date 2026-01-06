0b3f49cd93da1b4c22380f4f249a434e03002f4d
Majd Roses <majdrose@gmail.com>
2026-01-06 12:36:27 +0300

    Designing the overall structure of the To-Do List component

---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,44 @@
+import Container from "@mui/material/Container";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import { useState } from "react";
+export default () => {
+  const [alignment, setAlignment] = useState("web");
+
+  const handleChange = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
+  return (
+    <Container maxWidth="sm">
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography variant="h3">My Tasks</Typography>
+          <Divider />
+          {/* Fillter Btons */}
+          <ToggleButtonGroup
+            color="primary"
+            value={alignment}
+            exclusive
+            onChange={handleChange}
+            aria-label="Platform"
+            sx={{ marginTop: 2 }}
+          >
+            <ToggleButton value="all">All</ToggleButton>
+            <ToggleButton value="to_do">To Do</ToggleButton>
+            <ToggleButton value="completed">Done</ToggleButton>
+          </ToggleButtonGroup>
+          {/* ===/ Fillter Btons /==== */}
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </Container>
+  );
+};
